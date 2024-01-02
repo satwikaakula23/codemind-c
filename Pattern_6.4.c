d9eef5b1b0ccb042249a5e639bb7c10c403d99c0
satwikaakula23 <147487862+satwikaakula23@users.noreply.github.com>
2024-01-03 00:51:20 +0530

    #include<stdio.h> int main() {     int i,j,k,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(k=1;k<=i-1;k++)         {          printf(" ");         }         for(j=i;j<=n;j++)         {             printf("%c ",i+64);         }         printf(" ");      } }

---
+++ Pattern_6.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,n;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(k=1;k<=i-1;k++)
+        {
+         printf(" "); 
+        }
+        for(j=i;j<=n;j++)
+        {
+            printf("%c ",i+64);
+        }
+        printf("
+");
+     }
+}
